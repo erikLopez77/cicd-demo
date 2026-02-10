2ce25ba414c9aab01a9b34ccfba4512f749fdcc1
ErikLopez <espinozalopezerik@gmail.com>
2026-02-10 16:33:20 -0600

    generamos error para verlo en testing

--- src/test/groovy/codes/recursive/HelloWorldSpec.groovy
+++ src/test/groovy/codes/recursive/HelloWorldSpec.groovy
@@ -4,11 +4,18 @@ import io.micronaut.test.extensions.spock.annotation.MicronautTest
 
 @MicronautTest
 class HelloWorldSpec extends AbstractSpec{
-    def "test hello "(){
+    /* def "test hello "(){
         def foo='bar'
         when:
         foo=='bar'
         then:
         foo.reverse()=='rab'
+    } */
+    //provocar error
+    def "test failure" (){
+        when:
+        true==true
+        then:
+        false==true
     }
 }
